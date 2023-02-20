d0048da1360d2f784ee2c8175e2e93d1a1f083f2
jpcifnov18 <55262268+jpcifnov18@users.noreply.github.com>
2023-02-20 17:20:59 -0600

    Update installdevtools.bash

--- installdevtools.bash
+++ installdevtools.bash
@@ -1,10 +1,13 @@
 -------------------
 #instalar aws cli2
+echo Instalando aws cli2
 sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 -------------------
 #Instalar npm instal
+echo instalando npm
 sudo apt install npm -y
+instalando typescript
 npm install typescript
