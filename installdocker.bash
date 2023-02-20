771cd6e75543f618b657226954ee77508927e73c
juan.cifuentesn <juan.cifuentesn@bam.com.gt>
2023-02-20 15:01:52 -0600

    install-cambios

--- installdocker.bash
+++ installdocker.bash
@@ -1,19 +1,26 @@
 # actualice el repositorio de paquetes
+echo Actualizando el repositorio
 sudo apt-get update
+echo Instalando paquetes de requisitos
 #instalar paquetes de requisitos previos
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+echo Agregando Clave GPG
 #Agregar clave GPG
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo Agregando repositorio de Docker
 #Agregar repositorio de Docker
 sudo add-apt-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+echo Especificando fuente de instalación
 #Especifique la fuente de instalación
 apt-cache policy docker-ce
+echo Instalando Docker
 #Instalar docker
 sudo apt install docker-ce -y
 #Pasos despues de instalar docker
 #sudo groupadd docker
+echo Agregando usuario
 USER=$(whoami)
 sudo usermod -aG docker $USER
 newgrp docker
-echo USER
+echo $USER
 #Version de docker
