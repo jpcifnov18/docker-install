6e26a17dc0385ce85f011c138d4f5dddf1e783e4
jpcifnov18 <55262268+jpcifnov18@users.noreply.github.com>
2023-02-22 11:11:48 -0600

    ss

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 # Instalando Docker
 bash installdocker.bash
-# Agregar Grupo
-bash addgroup.bash
+
 # Instalando AWS , npm , Ts, 
 bash installdevtools.bash
 # Ejecutar docker
 bash rundocker.bash
+# Agregar Grupo
+bash addgroup.bash
